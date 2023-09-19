c0e03230ddbd686e614795f2375bfe0e618f657a
youngseo cho <dudtj0412@naver.com>
2023-09-19 21:41:36 +0900

    [MOD] 2559번 코드 수정

--- Python/2559.py
+++ Python/2559.py
@@ -5,10 +5,12 @@ n, k = map(int,input().split())
 temp = list(map(int,input().split()))
 
 part = sum(temp[:k]) #첫번째 경우의 합
-ans = 0
+ans = part
 
 for i in range(0, n-k): #가능한 경우의 수
     part += temp[i+k] - temp[i] #슬라이딩 윈도우
     ans = max(ans, part)
 
 print(ans) #최댓값 출력
+
+#해설 : https://y-seo.tistory.com/entry/%EB%B0%B1%EC%A4%80%ED%8C%8C%EC%9D%B4%EC%8D%AC-2559%EB%B2%88-%EC%88%98%EC%97%B4
